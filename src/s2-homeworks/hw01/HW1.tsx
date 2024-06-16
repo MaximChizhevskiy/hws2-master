7dec91477fcce97c1e21363991caaf75dd26d021
Maxim Chizhevskiy <max15.91@mail.ru>
2024-06-16 13:36:19 +0500

    hw1 is done

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -14,17 +14,28 @@ import avatar from './avatar.png'
 * */
 
 // нужно создать правильный тип вместо any
-export type MessageType = any
+export type MessageType = {
+    id: number,
+    user: {
+        avatar: string,
+        name: string,
+    },
+    message: {
+        text: string,
+        time: string
+    }
+
+}
 
 // структуру объекта не менять
 export const message0: MessageType = {
     id: 0,
     user: {
         avatar: avatar, // можно менять
-        name: 'Some Name',  // можно менять
+        name: 'You',  // можно менять
     },
     message: {
-        text: 'some textsome textsome textsome textsome textsome textsome text', // можно менять
+        text: 'Привет! Как дела? Чем занимаешься? Вопросов накопилось :)', // можно менять
         time: '22:00', // можно менять
     },
 }
@@ -32,11 +43,11 @@ export const friendMessage0: MessageType = {
     id: 100,
     user: {
         avatar: avatar, // можно менять
-        name: 'Friend Name', // можно менять
+        name: 'Your friend', // можно менять
     },
     message: {
-        text: 'зеркальное сообщение для тренировки css', // можно менять
-        time: '22:00', // можно менять
+        text: 'ООО! Привеет! Конечно, новости надо обсудить)', // можно менять
+        time: '22:01', // можно менять
     },
 }
 
@@ -47,7 +58,7 @@ const HW1 = () => {
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
-                    <Message message={message0} />
+                    <Message message={message0}/>
                     <FriendMessage message={friendMessage0} />
                 </div>
 
